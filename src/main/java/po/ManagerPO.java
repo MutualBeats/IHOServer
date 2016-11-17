aaedd3c69c0dc9a0c55da1b876d932cd96b6ff0e
Armour GU <779675978@qq.com>
2016-11-17 22:36:27 +0800

    Server端

---
+++ src/main/java/po/ManagerPO.java
@@ -0,0 +1,34 @@
+package po;
+
+public class ManagerPO {
+	/*
+	 * 网站管理人员ID
+	 */
+	private String managerID;
+	/*
+	 * 网站营销管理人员姓名
+	 */
+	private String managername;
+	/*
+	 * 构造方法
+	 * @param managerID
+	 * @param managername
+	 */
+	public ManagerPO(String managerID, String managername) {
+		super();
+		this.managerID = managerID;
+		this.managername = managername;
+	}
+	public String getManagerID() {
+		return managerID;
+	}
+	public void setManagerID(String managerID) {
+		this.managerID = managerID;
+	}
+	public String getManagername() {
+		return managername;
+	}
+	public void setManagername(String managername) {
+		this.managername = managername;
+	}
+}

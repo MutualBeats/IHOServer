e6ae8f0bb07a11ba9f1b74b47b8084e45ee88bf4
YYM <979579683@qq.com>
2016-11-25 00:37:41 +0800

    更新界面Controller

--- src/main/java/rmihelper/QuickStartController.java
+++ src/main/java/rmihelper/QuickStartController.java
@@ -51,7 +51,7 @@ public class QuickStartController implements Initializable{
 	
 	@FXML
 	public void handleExit() {
-		System.out.println("Exit");
+		sendMessage("Exit");
 		helper.off();
 		System.exit(0);
 	}
@@ -68,19 +68,28 @@ public class QuickStartController implements Initializable{
 	
 	@FXML
 	public void handleAbout() {
-		
+		sendMessage("服务器开发人员：Heleninsa 、 Huang Xiao.\r\n"
+				+ "服务器界面版本 : 1.0 \r\n"
+				+ "更新日期 : 2016 11 24\r\n"
+				+ "特性 : 对服务器直接操作. \r\n  "
+				+ "2.0预览版 : \r\n"
+				+ "	增加Highlight\r\n"
+				+ "	增加对不同信息的显示格式\r\n");
 	}
 	
 	public void sendMessage(String message) {
-		this.message.appendText("----------------------------------\r\n");
+		splitLine();
 		SimpleDateFormat df=new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 		String time=df.format(new Date());
 		this.message.appendText(time);
 		this.message.appendText("\r\n\r\n");
 		this.message.appendText(message);
 		this.message.appendText("\r\n");
-		this.message.appendText("----------------------------------\r\n");
+		splitLine();
 	}
 	
+	private void splitLine() {
+		this.message.appendText("----------------------------------\r\n");
+	}
 	
 }

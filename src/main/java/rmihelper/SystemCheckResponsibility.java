180010fd4bce60edecd2707aec402f99e4165cc9
黄潇 <hx36w35@163.com>
2016-12-11 19:50:35 +0800

    界面恢复

--- src/main/java/rmihelper/SystemCheckResponsibility.java
+++ src/main/java/rmihelper/SystemCheckResponsibility.java
@@ -1,49 +1,49 @@
-//package rmihelper;
-//
-//import java.util.Timer;
-//import java.util.TimerTask;
-//
-//public class SystemCheckResponsibility {
-//
-//	private static Timer ORDER_REFRESH_SERVICE;
-//
-//	private static boolean startOrderBackRefreshServie() {
-//		if (ORDER_REFRESH_SERVICE != null) {
-//			QuickStart.sendMessage("You Must Close the Order Refresh Service");
-//			return false;
-//		}
-//		// 1000ms*60s*60min*24h = 86400000)
-//		ORDER_REFRESH_SERVICE = new Timer();
-//		ORDER_REFRESH_SERVICE.schedule(new OrderRefresh(), 0, 86400000);
-//		return true;
-//	}
-//
-//	private static boolean endOrderBackRefreshService() {
-//		if (ORDER_REFRESH_SERVICE == null) {
-//			QuickStart.sendMessage("No Order Refresh Service Exist");
-//			return false;
-//		}
-//		ORDER_REFRESH_SERVICE.cancel();
-//		ORDER_REFRESH_SERVICE = null;
-//		return true;
-//	}
-//
-//	static class OrderRefresh extends TimerTask {
-//		@Override
-//		public void run() {
-//			// The order refresh background service
-//			QuickStart.sendMessage("Refresh order service start");
-//			// TODO : Finish the order state refresh.
-//		}
-//
-//	}
-//
-//	public static boolean endBackRefreshService() {
-//		return startOrderBackRefreshServie();
-//	}
-//
-//	public static boolean startBackRefreshServie() {
-//		return endOrderBackRefreshService();
-//	}
-//
-//}
+package rmihelper;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SystemCheckResponsibility {
+
+	private static Timer ORDER_REFRESH_SERVICE;
+
+	private static boolean startOrderBackRefreshServie() {
+		if (ORDER_REFRESH_SERVICE != null) {
+			QuickStart.sendMessage("You Must Close the Order Refresh Service");
+			return false;
+		}
+		// 1000ms*60s*60min*24h = 86400000)
+		ORDER_REFRESH_SERVICE = new Timer();
+		ORDER_REFRESH_SERVICE.schedule(new OrderRefresh(), 0, 86400000);
+		return true;
+	}
+
+	private static boolean endOrderBackRefreshService() {
+		if (ORDER_REFRESH_SERVICE == null) {
+			QuickStart.sendMessage("No Order Refresh Service Exist");
+			return false;
+		}
+		ORDER_REFRESH_SERVICE.cancel();
+		ORDER_REFRESH_SERVICE = null;
+		return true;
+	}
+
+	static class OrderRefresh extends TimerTask {
+		@Override
+		public void run() {
+			// The order refresh background service
+			QuickStart.sendMessage("Refresh order service start");
+			// TODO : Finish the order state refresh.
+		}
+
+	}
+
+	public static boolean endBackRefreshService() {
+		return startOrderBackRefreshServie();
+	}
+
+	public static boolean startBackRefreshServie() {
+		return endOrderBackRefreshService();
+	}
+
+}
--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -1,15 +1,15 @@
-//package test;
-//
-//public class Test {
-//	
-//	public static void main(String[] args) {
-//		RMIHelperCopyright helper = RMIHelperCopyright.getInstance();
-//		System.out.println(helper.address);
-//		helper.run();
-//		
-//		helper.off();
-//		helper.run();
-//		helper.off();
-//	}
-//	
-//}
+package test;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		RMIHelperCopyright helper = RMIHelperCopyright.getInstance();
+		System.out.println(helper.address);
+		helper.run();
+		
+		helper.off();
+		helper.run();
+		helper.off();
+	}
+	
+}
